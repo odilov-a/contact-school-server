375954d08296440c761bdc50b5f7832977a5f194
odilov-a <akbarjonodilov01@gmail.com>
2024-03-20 11:44:04 +0500

    fix: backup

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cron = require("node-cron");
 const mainRouter = require("./routes/router.js");
 require("./backup.js");
 const video = require("./video.js");
 const path = require("path");
 const fs = require("fs");
 const PORT = process.env.PORT || 3005;
-dotenv.config();
 const app = express();
 app.use(express.json());
 
 const logDir = path.join(__dirname, "logs");
-
 function getCurrentDateTime() {
   const now = new Date();
   return `${now.getFullYear()}-${padNumber(now.getMonth() + 1)}-${padNumber(
     now.getDate()
   )}`;
 }
-
 function padNumber(num) {
   return num.toString().padStart(2, "0");
 }
-
 const logFilePath = path.join(logDir, `${getCurrentDateTime()}.mongodb.log`);
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
@@ -45,20 +41,7 @@ mongoose
   .catch((err) => console.log("db error", err.message));
 
 app.use(cors());
-
 app.get("/video/:video", video);
-
-cron.schedule(
-  "0 0 * * *",
-  () => {
-    console.log("Running backup job...");
-    getBackup();
-  },
-  {
-    scheduled: true,
-    timezone: "Asia/Tashkent",
-  }
-);
 
 app.use("/api", mainRouter);
 app.use("/uploads", express.static("uploads"));
